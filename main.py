46335b6b58cd7f9a8ce48ffa9a5df7a67c413f86
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-02-28 04:32:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,8 @@ async def on_ready():
 @bot.event
 async def on_message(message):
   # Ignore messages sent by the bot
-  # if message.author == bot.user and message.author.id != DEVELOPER_ID:
-  #   return
+  if message.author == bot.user:
+    return
  # if isinstance(message.channel, discord.DMChannel) and message.author.id != DEVELOPER_ID:
  #    # await message.author.send("Sorry, I'm currently not accepting direct messages.")
  #   return
